5ed29c936dfc16a490f4e8e3d75c30d87a416f68
Matt Morris <mmorris515@gmail.com>
2025-05-08 09:42:35 -0700

    Add request module import

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
