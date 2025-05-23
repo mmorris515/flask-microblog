f6c579a514b952101c0993c1912ca2b1f29e7213
Matt Morris <mmorris515@gmail.com>
2025-05-23 16:01:57 -0700

    Configure Flask-Mail with Gmail

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, ".env"))  # Load variables from .env
+
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
@@ -7,10 +11,11 @@ class Config:
         'sqlite:///' + os.path.join(basedir, 'app.db')
     POSTS_PER_PAGE = 3
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+
+
 
     
