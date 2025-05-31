2ad4667a6f976a54928d1b62514ef95f6253796f
Matt Morris <mmorris515@gmail.com>
2025-05-31 10:46:40 -0700

    Add French as language option

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ import os
 
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-    # return 'es'
+    # return 'fr'
 
 app = Flask(__name__)
 app.config.from_object(Config)
