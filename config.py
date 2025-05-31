bbad1fb5f314e0574e68878ca518ada5027d9cca
Matt Morris <mmorris515@gmail.com>
2025-05-31 10:26:02 -0700

    Implement localization and enable spanish translation

--- config.py
+++ config.py
@@ -15,6 +15,7 @@ class Config:
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    LANGUAGES = ['en', 'es']
 
 
 
