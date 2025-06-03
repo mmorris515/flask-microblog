8daa0bceecd244b24701214deabd0d557a00a018
Matt Morris <mmorris515@gmail.com>
2025-06-03 15:49:45 -0700

    Implement post translation

--- config.py
+++ config.py
@@ -1,23 +1,18 @@
 import os
-from dotenv import load_dotenv
-
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, ".env"))  # Load variables from .env
 
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-    POSTS_PER_PAGE = 3
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
     LANGUAGES = ['en', 'es']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
-
-
-
-    
+    MS_TRANSLATOR_REGION = os.environ.get('MS_TRANSLATOR_REGION')
+    POSTS_PER_PAGE = 25
